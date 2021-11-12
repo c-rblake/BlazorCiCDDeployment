f421b38b1bc2b0479bc19a27ea7c1bb655ef843b
Christoper <c.blake@hotmail.se>
2021-11-12 15:14:01 +0100

    Swapping to API since Blazor is bad at Delegates and complex structures. Requiring certain code order.

--- BlazorCiCDDeployment/Program.cs
+++ BlazorCiCDDeployment/Program.cs
@@ -19,7 +19,8 @@ namespace BlazorCiCDDeployment
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://machinefuncapi20211107093917.azurewebsites.net")
+            });
             builder.Services.AddSingleton<IMachineList, MachineList>();
 
             await builder.Build().RunAsync();
